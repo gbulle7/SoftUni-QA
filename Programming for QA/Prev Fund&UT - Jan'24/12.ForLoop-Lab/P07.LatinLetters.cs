323d7bdec74b2aeac24728cb15455a3f2687c7ee
gbulle7 <144064831+gbulle7@users.noreply.github.com>
2024-05-01 11:46:36 +0300

    Previous Programming for QA Course

---
+++ Programming for QA/Prev Fund&UT - Jan'24/12.ForLoop-Lab/P07.LatinLetters.cs
@@ -0,0 +1,7 @@
+char firstLetter = char.Parse(Console.ReadLine());
+char lastLetter = char.Parse(Console.ReadLine());
+
+for (char x = firstLetter; x <= lastLetter; x++)
+{
+    Console.Write(x + " ");
+}
