002da4a86ffa085758f60f5b26b5285fa1cac7ff
gbulle7 <144064831+gbulle7@users.noreply.github.com>
2024-05-18 03:32:18 +0300

    Arrays and Lists - Exercises

--- Programming for QA/Programming Fundamentals and Unit Testing May'24/20.Arrays&Lists-Exercises/P08.ArrayRotation.cs
+++ Programming for QA/Programming Fundamentals and Unit Testing May'24/20.Arrays&Lists-Exercises/P08.ArrayRotation.cs
@@ -8,3 +8,21 @@ for (int i = 0; i < num; i++)
     seq.Add(pop);
 }
 Console.WriteLine(string.Join(" ", seq));
+
+/*
+int[] seq = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < num; i++)
+{
+    int tempInt = seq[0];
+    for (int j = 0; j < seq.Length - 1; j++)
+    {
+        
+        seq[j] = seq[j + 1];
+    }
+    seq[seq.Length - 1] = tempInt;
+}
+
+Console.WriteLine(string.Join(" ", seq));
+*/
